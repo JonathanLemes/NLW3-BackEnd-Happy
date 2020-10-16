50fa5f7181fcbd155c13b87eafbe0088b212a709
JonathanLemes <JonathanLemes@unifei.edu.br>
2020-10-16 02:11:02 -0300

    ws02
    WorkShop 02 finalizado.

---
+++ src/config/upload.ts
@@ -0,0 +1,13 @@
+import multer, { diskStorage } from 'multer';
+import path from 'path';
+
+export default {
+    storage: multer.diskStorage({
+        destination: path.join(__dirname, '..', '..', 'uploads'),
+        filename: (request, file, callback) => {
+            const fileName = `${Date.now()}-${file.originalname}`;
+
+            callback(null, fileName);
+        }
+    })
+}
